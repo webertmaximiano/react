903acf55f6c26942359a7f594d4579c0ffb54e42
Webert Maximiano <webertcoach@gmail.com>
2023-07-15 20:38:31 -0300

    render ul

--- semana01/index.js
+++ semana01/index.js
@@ -1,1 +1,9 @@
 ReactDOM.render(<h1>Hello, Clube Siga!</h1>, document.getElementById("root"))
+
+ReactDOM.render(
+    <ul>
+        <li>Thing 1</li>
+        <li>Thing 2</li>
+    </ul>, 
+    document.getElementById("ul")
+)
