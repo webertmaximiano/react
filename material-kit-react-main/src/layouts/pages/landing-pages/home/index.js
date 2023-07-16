5c1c90a718702191320ccff169387b377e6611ee
Webert Maximiano <webertcoach@gmail.com>
2023-07-16 14:42:31 -0300

    add material kit

---
+++ material-kit-react-main/src/layouts/pages/landing-pages/home/index.js
@@ -0,0 +1,15 @@
+
+import React from "react";
+import Navbar from "components/Navbar";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>Bem-vindo à Minha Página Inicial</h1>
+      <p>Este é um exemplo de página inicial usando o Material Kit 2 React.</p>
+    </div>
+  );
+};
+
+export default Home;
