5f94a1e0af3c978ecb8e09414e0a10ac868b61a5
Webert Maximiano <webertcoach@gmail.com>
2023-07-17 16:49:48 -0300

    Project: Map experiences data into components

---
+++ semana02/Mapping components/App.js
@@ -0,0 +1,40 @@
+import React from "react"
+import Joke from "./Joke"
+
+
+export default function App() {
+    
+    return (
+        <div>
+            
+        </div>
+    )
+}
+            // <Joke 
+            //     punchline="It’s hard to explain puns to kleptomaniacs because they always take things literally."
+            // />
+            // <Joke 
+            //     setup="I got my daughter a fridge for her birthday." 
+            //     punchline="I can't wait to see her face light up when she opens it." 
+            //     isPun={false}
+            // />
+            // <Joke 
+            //     setup="How did the hacker escape the police?" 
+            //     punchline="He just ransomware!"
+            //     isPun={true}
+            // />
+            // <Joke 
+            //     setup="Why don't pirates travel on mountain roads?" 
+            //     punchline="Scurvy." 
+            //     isPun={true}
+            // />
+            // <Joke 
+            //     setup="Why do bees stay in the hive in the winter?" 
+            //     punchline="Swarm." 
+            //     isPun={true}
+            // />
+            // <Joke 
+            //     setup="What's the best thing about Switzerland?" 
+            //     punchline="I don't know, but the flag is a big plus!" 
+            //     isPun={false}
+            // />
