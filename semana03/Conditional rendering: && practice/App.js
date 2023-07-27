043df3cfce1341cd66a478f0da56ffc43eee6ee7
Webert Maximiano <webertcoach@gmail.com>
2023-07-27 15:09:37 -0300

    Conditional rendering: && practice

---
+++ semana03/Conditional rendering: && practice/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function App() {
+    const [messages, setMessages] = React.useState(["a", "b"])
+    /**
+     * Challenge:
+     * - Only display the <h1> below if there are unread messages
+     */
+    return (
+        <div>
+            <h1>You have _ unread messages!</h1>
+        </div>
+    )
+}
