f7727baa868841fe96f06b0bf6c1293da7ad3b9d
Webert Maximiano <webertcoach@gmail.com>
2023-07-27 14:11:02 -0300

    Dynamic styles

---
+++ semana03/Dynamic styles/App.js
@@ -0,0 +1,19 @@
+import React from "react"
+import boxes from "./boxes"
+
+export default function App(props) {
+    const [squares, setSquares] = React.useState(boxes)
+    
+    const styles = {
+        backgroundColor: props.darkMode ? "#222222" : "#cccccc"
+    }
+    
+    const squareElements = squares.map(square => (
+        <div style={styles} className="box" key={square.id}></div>
+    ))
+    return (
+        <main>
+            {squareElements}
+        </main>
+    )
+}
