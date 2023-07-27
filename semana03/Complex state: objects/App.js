3c1e01f6a39fbdf510dd8f2612da3e4889d63a37
Webert Maximiano <webertcoach@gmail.com>
2023-07-27 13:28:19 -0300

    refactor state

---
+++ semana03/Complex state: objects/App.js
@@ -0,0 +1,41 @@
+import React from "react"
+
+export default function App() {
+    const [contact, setContact] = React.useState({
+        firstName: "John",
+        lastName: "Doe",
+        phone: "+1 (719) 555-1212",
+        email: "itsmyrealname@example.com",
+        isFavorite: false
+    })
+    /**
+     * Challenge: Fill in the values in the markup
+     * using the properties of our state object above
+     * (Ignore `isFavorite` for now)
+     */
+    
+    function toggleFavorite() {
+        console.log("Toggle Favorite")
+    }
+    
+    return (
+        <main>
+            <article className="card">
+                <img src="./images/user.png" className="card--image" />
+                <div className="card--info">
+                    <img 
+                        src={`../images/star-empty.png`} 
+                        className="card--favorite"
+                        onClick={toggleFavorite}
+                    />
+                    <h2 className="card--name">
+                        John Doe
+                    </h2>
+                    <p className="card--contact">+1 (719) 555-1212</p>
+                    <p className="card--contact">itsmyrealname@example.com</p>
+                </div>
+                
+            </article>
+        </main>
+    )
+}
