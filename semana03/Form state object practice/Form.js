dbd06d6f9d4f28e2d838608148e25b6015f28d09
Webert Maximiano <webertcoach@gmail.com>
2023-07-27 15:53:24 -0300

    Controlled inputs

---
+++ semana03/Form state object practice/Form.js
@@ -0,0 +1,39 @@
+import React from "react"
+
+export default function Form() {
+    const [formData, setFormData] = React.useState(
+        {firstName: "", lastName: ""}
+    )
+    
+    /**
+     * Challenge: add an email field/state to the form
+     */
+    
+    console.log(formData)
+    
+    function handleChange(event) {
+        setFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                [event.target.name]: event.target.value
+            }
+        })
+    }
+    
+    return (
+        <form>
+            <input
+                type="text"
+                placeholder="First Name"
+                onChange={handleChange}
+                name="firstName"
+            />
+            <input
+                type="text"
+                placeholder="Last Name"
+                onChange={handleChange}
+                name="lastName"
+            />
+        </form>
+    )
+}
