719deda2919027c2d71e23a600fce2413dd30a0f
Webert Maximiano <webertcoach@gmail.com>
2023-07-28 13:56:35 -0300

    Tenzies: Die components

---
+++ semana04/Tenzies: Die components/App.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+/**
+ * Challenge:
+ * 
+ * - Create a Die component that takes a `value` prop
+ * - Render 10 instances of the Die component (manually)
+ *      - Provide a number between 1-6 for the value on each
+ *        for now
+ * - Style the <main> and <Die> components 
+ *   to look like they do in the slide
+ */
+
+export default function App() {
+    return (
+        <main></main>
+    )
+}
