c58de55ae343f3dd033f49d79b8cbfa76f08edbe
Webert Maximiano <webertcoach@gmail.com>
2023-07-28 11:27:04 -0300

    useEffect cleanup function

---
+++ semana03/State and Effect practices/App.js
@@ -0,0 +1,20 @@
+import React from "react"
+import WindowTracker from "./WindowTracker"
+
+export default function App() {
+    /**
+     * Challenge:
+     * 1. Create state called `show`, default to `true`
+     * 2. When the button is clicked, toggle `show`
+     * 3. Only display `<WindowTracker>` if `show` is `true`
+     */
+    
+    return (
+        <div className="container">
+            <button>
+                Toggle WindowTracker
+            </button>
+            <WindowTracker />
+        </div>
+    )
+}
