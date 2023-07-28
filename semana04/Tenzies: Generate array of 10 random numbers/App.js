69b75ef3e6dadf09cd87bf521f444b430c0dcc29
Webert Maximiano <webertcoach@gmail.com>
2023-07-28 13:58:52 -0300

    Tenzies: Generate array of 10 random numbers

---
+++ semana04/Tenzies: Generate array of 10 random numbers/App.js
@@ -0,0 +1,30 @@
+import React from "react"
+import Die from "./Die"
+
+/**
+ * Challenge:
+ * 
+ * Write a function (allNewDice) that returns an array 
+ * of 10 random numbers between 1-6 inclusive.
+ * 
+ * Log the array of numbers to the console for now
+ */
+
+export default function App() {
+    return (
+        <main>
+            <div className="dice-container">
+                <Die value="1" />
+                <Die value="2" />
+                <Die value="3" />
+                <Die value="4" />
+                <Die value="5" />
+                <Die value="6" />
+                <Die value="1" />
+                <Die value="1" />
+                <Die value="1" />
+                <Die value="1" />
+            </div>
+        </main>
+    )
+}
